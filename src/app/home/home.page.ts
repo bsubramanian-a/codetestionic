426211950e174888c1fbbf4f27b7622e5fa610a9
Rajavel Balasubramanian <rajavel.balasubramanian@bloksol.com>
2021-07-16 18:24:33 +0530

    online status changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService, Product } from '../services/data.service';
+import { Network } from '@ionic-native/network/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,28 @@ import { DataService, Product } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  onlineStatus:string = "offline";
-  constructor(private data: DataService) {
+  onlineStatus:string;
+  constructor(private data: DataService,private network: Network) {
+
+    let disconnectSubscription = this.network.onDisconnect().subscribe(() => {
+      console.log('network was disconnected :-(');
+      this.onlineStatus = "network was disconnected :-(";
+    });
+
+    let connectSubscription = this.network.onConnect().subscribe(() => {
+      console.log('network connected!');
+      this.onlineStatus = "network connected!";
+      // We just got a connection but we need to wait briefly
+       // before we determine the connection type. Might need to wait.
+      // prior to doing any api requests as well.
+      setTimeout(() => {
+        if (this.network.type === 'wifi') {
+          this.onlineStatus = "we got a wifi connection, woohoo!";
+          console.log('we got a wifi connection, woohoo!');
+        }
+      }, 3000);
+    });
+
     if (navigator.onLine) {
       this.onlineStatus = "online";
     }
